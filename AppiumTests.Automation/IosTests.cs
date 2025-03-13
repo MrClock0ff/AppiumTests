ce18e986efb5e6366614bb5cd8663294ebac58f4
MrClock0ff <mrclockoff@hotmail.com>
2025-03-13 13:39:27 +0000

    Coordinate touch

--- AppiumTests.Automation/IosTests.cs
+++ AppiumTests.Automation/IosTests.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Enums;
@@ -6,6 +7,8 @@ using OpenQA.Selenium.Appium.Interfaces;
 using OpenQA.Selenium.Appium.iOS;
 using OpenQA.Selenium.BiDi.Modules.Input;
 using OpenQA.Selenium.DevTools.V132.Input;
+using OpenQA.Selenium.Interactions;
+using PointerInputDevice = OpenQA.Selenium.Appium.Interactions.PointerInputDevice;
 using TouchPoint = OpenQA.Selenium.DevTools.V131.Input.TouchPoint;
 
 namespace AppiumTests.Automation;
@@ -15,8 +18,8 @@ public class Tests
 	private const string DeviceName = "iPhone 15 Pro Max iOS 17.5";
 	private const string DevicePlatformVersion = "17.5";
 	private const string DeviceUid = "3A0915CB-48FE-4657-957F-E4F15542105F";
-	private const string AppPath = "~/Programming/Repos/AppiumTests/AppiumTests.iOS/bin/Debug/net8.0-ios/iossimulator-arm64/AppiumTests.iOS.app";
-	private const string WdaPath = "~/Library/Developer/Xcode/DerivedData/WebDriverAgent-ddibmffcftnvrcfikoigguraxuzz/Build/Products/Debug-iphoneos/WebDriverAgentRunner-Runner.app";
+	private const string AppPath = "/Users/danielklokov/Programming/Repos/AppiumTests/AppiumTests.iOS/bin/Debug/net8.0-ios/iossimulator-arm64/AppiumTests.iOS.app";
+	private const string WdaPath = "/Users/danielklokov/Library/Developer/Xcode/DerivedData/WebDriverAgent-ddibmffcftnvrcfikoigguraxuzz/Build/Products/Debug-iphoneos/WebDriverAgentRunner-Runner.app";
 	private const string ServiceUrl = "http://127.0.0.1:4723";
 
 	private IOSDriver? _driver;
@@ -36,7 +39,7 @@ public class Tests
 		driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.Udid, DeviceUid);
 		driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.NoReset, true);
 		driverOptions.AddAdditionalAppiumOption(MobileCapabilityType.FullReset, false);
-		driverOptions.AddAdditionalAppiumOption(IOSMobileCapabilityType.AutoDismissAlerts, true);
+		//driverOptions.AddAdditionalAppiumOption(IOSMobileCapabilityType.AutoDismissAlerts, true);
 		//driverOptions.AddAdditionalAppiumOption(IOSMobileCapabilityType.AutoAcceptAlerts, true);
 
 		driverOptions.AddAdditionalAppiumOption("usePreinstalledWDA", false);
@@ -57,10 +60,29 @@ public class Tests
 	[Test]
 	public void RunApp_Test()
 	{
-		AppiumElement? btn = _driver?.FindElement(By.Id("Show Location Popup"));
-		btn?.Click();
+		// AppiumElement? btn = _driver?.FindElement(By.Id("Show Location Popup"));
+		// btn?.Click();
+		
+		Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+
+		Size screenSize = _driver?.Manage().Window.Size ?? Size.Empty;
+		PointerInputDevice inputDevice = new PointerInputDevice(PointerKind.Touch);
+
+		ActionSequence btnTouch = new ActionSequence(inputDevice);
+		btnTouch.AddAction(inputDevice.CreatePointerMove(CoordinateOrigin.Viewport, screenSize.Width / 2, screenSize.Height / 2, TimeSpan.FromSeconds(1)));
+		btnTouch.AddAction(inputDevice.CreatePointerDown(PointerButton.TouchContact));
+		btnTouch.AddAction(inputDevice.CreatePointerUp(PointerButton.TouchContact));
+
+		_driver?.PerformActions([btnTouch]);
 
 		Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+
+		ActionSequence dialogBtnTouch = new ActionSequence(inputDevice);
+		dialogBtnTouch.AddAction(inputDevice.CreatePointerMove(CoordinateOrigin.Viewport, screenSize.Width / 2, (screenSize.Height / 3) * 2, TimeSpan.FromSeconds(1)));
+		dialogBtnTouch.AddAction(inputDevice.CreatePointerDown(PointerButton.TouchContact));
+		dialogBtnTouch.AddAction(inputDevice.CreatePointerUp(PointerButton.TouchContact));
+
+		_driver?.PerformActions([dialogBtnTouch]);
 
 		// AppiumElement? alertBtn = _driver?.FindElement(By.Id("Allow Once"));
 		// alertBtn?.Click();
